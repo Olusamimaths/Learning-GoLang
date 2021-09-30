1044a7ed1c0c7e1ed449b03f9cf67a556c7e2ae1
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2021-09-30 10:11:47 +0100

    :fire: More Handling errors

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -29,7 +29,7 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string ,error) {
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
 		http.NotFound(w, r)
-		return "", errors.New("Invalid page title")
+		return "", errors.New("invalid page title")
 	}
 	return m[2], nil // The title is the second subexpression
 }
@@ -51,7 +51,11 @@ func renderTemplate(w http.ResponseWriter, tempstr string, p *Page) {
 	}
 }
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):] // /view/TestPage => TestPage
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
+
 	p, err := loadPage(title)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -61,7 +65,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
@@ -71,11 +78,14 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+	title, err := getTitle(w, r)
+	if err != nil {
+		return
+	}
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	
-	err := p.save()
+	err = p.save()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
