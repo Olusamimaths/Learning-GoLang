d4b19e56046fb599c10d02fee308731ea0740264
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2021-10-01 13:18:29 +0100

    :fire: Completed chapter 9

---
+++ ch9/overriding_package_name.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	crand "crypto/rand"
+	"encoding/binary"
+	"math/rand"
+)
+
+func seedRand() *rand.Rand {
+	var b [8]byte
+	_, err := crand.Read(b[:])
+	if err != nil {
+		panic("cannot seed with crytographic random number generator")
+	}
+	r := rand.New(rand.NewSource(int64(binary.LittleEndian.Uint64(b[:]))))
+	return r;
+}
+
+func main() {
+	
+}
---
+++ ch9/third_party_code/go.mod
@@ -0,0 +1,8 @@
+module money
+
+go 1.16
+
+require (
+	github.com/learning-go-book/formatter v0.0.0-20200921021027-5abc380940ae
+	github.com/shopspring/decimal v1.2.0
+)
